e42f28f1e2a0e87766cad1ec1641f0ff56e05611
Debasish Das <160996215+Imdebasish18@users.noreply.github.com>
2024-06-12 12:57:52 +0530

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -83,7 +83,7 @@ exports.login = async (req, res) => {
     }
   } catch (error) {
     console.error(error);
-    res.status(500).send("An error occurred while registering the user");
+    res.status(500).send(error);
   }
 };
 
